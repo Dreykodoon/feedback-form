77f546e223d8e2a10d58f2f6a4f2830812112cdb
Mihai Cioban <mcioban@axway.com>
2017-09-22 14:55:22 +0300

    Sending form values to the back-end.

--- src/home/feedback-form.js
+++ src/home/feedback-form.js
@@ -3,24 +3,48 @@ import TextField from 'material-ui/TextField';
 import Button from 'material-ui/Button';
 import axios from 'axios';
 
-function sendTestData() {
-    axios({
-        method: 'post',
-        url:    '/app',
-        data:   {
-            test: 'test'
-        }
-    });
-}
+class FeedbackForm extends React.Component {
+    constructor() {
+        super();
 
-const FeedbackForm = () => (
-    <div>
-        <TextField label='Name' fullWidth margin='normal'/>
-        <TextField label='Email' fullWidth margin='normal'/>
-        <TextField label='Subject' fullWidth margin='normal'/>
-        <TextField rows='5' rowsMax='5' label='Message' multiline fullWidth margin='normal'/>
-        <Button raised onClick={sendTestData}>Submit</Button>
-    </div>
-);
+        this.state = {
+            name:    '',
+            email:   '',
+            subject: '',
+            message: '',
+        };
+    }
+
+    sendTestData() {
+        axios({
+            method: 'post',
+            url:    '/app',
+            data:   this.state,
+        }).then(() => console.log('response received'));
+    }
+
+    handleChange(fieldName) {
+        return (event) => {
+            this.setState({
+                [fieldName]: event.target.value,
+            });
+        };
+    }
+
+    render() {
+        const {name, email, subject, message} = this.state;
+
+        return (
+            <div>
+                <TextField onChange={this.handleChange('name')} value={name} label='Name' fullWidth margin='normal'/>
+                <TextField onChange={this.handleChange('email')} value={email} label='Email' fullWidth margin='normal'/>
+                <TextField onChange={this.handleChange('subject')} value={subject} label='Subject' fullWidth margin='normal'/>
+                <TextField onChange={this.handleChange('message')} value={message} rows='5' rowsMax='5' label='Message' multiline fullWidth margin='normal'/>
+                <Button raised onClick={this.sendTestData.bind(this)}>Submit</Button>
+            </div>
+        );
+
+    }
+}
 
 export default FeedbackForm;
