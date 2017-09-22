74f13049256110f62c0f07419e9a7b8eb22c5311
Mihai Cioban <mcioban@axway.com>
2017-09-22 12:16:38 +0300

    Added axios for HTTP requests

--- src/home/feedback-form.js
+++ src/home/feedback-form.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import TextField from 'material-ui/TextField';
+import Button from 'material-ui/Button';
+import axios from 'axios';
+
+function sendEmail() {
+    axios({
+        method: 'post',
+        url:    '/test',
+        data:   {
+            test: 'test'
+        }
+    });
+}
 
 const FeedbackForm = () => (
     <div>
-        <TextField label='Name' fullWidth margin='normal' />
-        <TextField label='Email' fullWidth margin='normal' />
-        <TextField label='Subject' fullWidth margin='normal' />
-        <TextField rows='5' rowsMax='5' label='Message' multiline fullWidth margin='normal' />
+        <TextField label='Name' fullWidth margin='normal'/>
+        <TextField label='Email' fullWidth margin='normal'/>
+        <TextField label='Subject' fullWidth margin='normal'/>
+        <TextField rows='5' rowsMax='5' label='Message' multiline fullWidth margin='normal'/>
+        <Button raised onClick={sendEmail}>Submit</Button>
     </div>
 );
 
