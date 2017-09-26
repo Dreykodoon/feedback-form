ef32c2ac5a09ff02eddcf56aa50a6f8ebf864b15
Mihai Cioban <mcioban@axway.com>
2017-09-26 08:32:18 +0300

    Moved snackbar into the feedback form component.

--- src/home/feedback-form.js
+++ src/home/feedback-form.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import TextField from 'material-ui/TextField';
 import Button from 'material-ui/Button';
+import Snackbar from 'material-ui/Snackbar';
+import {withStyles} from 'material-ui/styles';
 import axios from 'axios';
+
+const styles = {
+    snackbarMessage: {
+        color:     '#36bd5a',
+        textAlign: 'center',
+        width:     '100%',
+    }
+};
 
 class FeedbackForm extends React.Component {
     constructor() {
         super();
 
         this.state = {
+            snackbarOpen: true,
             name:    '',
             email:   '',
             subject: '',
@@ -31,8 +43,13 @@ class FeedbackForm extends React.Component {
         };
     }
 
+    handleSnackbarClose() {
+        this.setState({snackbarOpen: false});
+    }
+
     render() {
         const {name, email, subject, message} = this.state;
+        const {classes} = this.props;
 
         return (
             <div>
@@ -41,10 +58,15 @@ class FeedbackForm extends React.Component {
                 <TextField onChange={this.handleChange('subject')} value={subject} label='Subject' fullWidth margin='normal'/>
                 <TextField onChange={this.handleChange('message')} value={message} rows='5' rowsMax='5' label='Message' multiline fullWidth margin='normal'/>
                 <Button raised onClick={this.sendTestData.bind(this)}>Submit</Button>
+                <Snackbar anchorOrigin={{ vertical: 'top', horizontal: 'center' }} open={this.state.snackbarOpen} onRequestClose={this.handleSnackbarClose.bind(this)}
+                          SnackbarContentProps={{classes: {message: classes.snackbarMessage}}} message='Welcome to my world!'/>
             </div>
         );
-
     }
 }
 
-export default FeedbackForm;
+FeedbackForm.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(FeedbackForm);
--- src/home/home.js
+++ src/home/home.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import Paper from 'material-ui/Paper';
 import Typography from 'material-ui/Typography';
 import Grid from 'material-ui/Grid';
-import Snackbar from 'material-ui/Snackbar';
-import {withStyles} from 'material-ui/styles';
 import Container from '../layout/container';
 import FeedbackForm from './feedback-form';
 
 const styles = {
-    body:            {
+    body:          {
         padding:         16,
         marginTop:       16,
         marginBottom:    16,
@@ -17,61 +14,34 @@ const styles = {
         maxWidth:        '1140px',
         display:         'flex',
     },
-    leftContainer:   {
+    leftContainer: {
         display:      'flex',
         flexWrap:     'wrap',
         alignContent: 'center',
-    },
-    snackbarContent: {
-        color:     '#36bd5a',
-        textAlign: 'center',
-        width:     '100%',
     }
 };
 
-class Home extends React.Component {
-    constructor() {
-        super();
-
-        this.state = {
-            snackbarOpen: true,
-        };
-    }
-
-    handleSnackbarClose() {
-        this.setState({snackbarOpen: false});
-    }
-
-    render() {
-        const {classes} = this.props;
-
-        return (<Container>
-            <Paper className={classes.body} elevation={1}>
-                <Grid container>
-                    <Grid item xs={12} sm={6} className={classes.leftContainer}>
-                        <Typography style={{width: '100%'}} type='display3' component='h1' gutterBottom align='center'>
-                            Feedback form
-                        </Typography>
-                        <Typography style={{width: '100%'}} component='p' gutterBottom align='center'>
-                            {`
+const Home = () => (
+    <Container>
+        <Paper style={styles.body} elevation={1}>
+            <Grid container>
+                <Grid item xs={12} sm={6} style={styles.leftContainer}>
+                    <Typography style={{width: '100%'}} type='display3' component='h1' gutterBottom align='center'>
+                        Feedback form
+                    </Typography>
+                    <Typography style={{width: '100%'}} component='p' gutterBottom align='center'>
+                        {`
                           This application is a playground for testing and learning new technologies.
                         `}
-                        </Typography>
-                    </Grid>
-                    <Grid item xs={12} sm={1}/>
-                    <Grid item xs={12} sm={5}>
-                        <FeedbackForm/>
-                    </Grid>
+                    </Typography>
                 </Grid>
-            </Paper>
-            <Snackbar anchorOrigin={{vertical: 'top', horizontal: 'center'}} open={this.state.snackbarOpen} onRequestClose={this.handleSnackbarClose.bind(this)}
-                      SnackbarContentProps={{classes: {message: classes.snackbarContent}}} message='Welcome to my world!'/>
-        </Container>);
-    }
-}
+                <Grid item xs={12} sm={1}/>
+                <Grid item xs={12} sm={5}>
+                    <FeedbackForm/>
+                </Grid>
+            </Grid>
+        </Paper>
+    </Container>
+);
 
-Home.propTypes = {
-    classes:  PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(Home);
+export default Home;
