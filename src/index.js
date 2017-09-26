5b225d3467c77781fbd90de76f1a73997dae67ea
Mihai Cioban <mcioban@axway.com>
2017-09-26 09:16:08 +0300

    Bumped material-ui version and removed unused dependencies.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 import App from './app';
 import './style/style.scss';
-
-injectTapEventPlugin();
 
 const app = document.createElement('div');
 document.body.appendChild(app);
