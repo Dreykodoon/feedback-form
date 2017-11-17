31ad0ff617f8b18d04f7069d5cbbea5374ea3a89
Mihai <ciobanmihai@gmail.com>
2017-11-17 13:58:44 +0200

    Added request for cookie creation. Anti form spam measure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import App from './app';
 import './style/style.scss';
+
+// Protection measure against directly spamming resources.
+axios({method: 'post', url: '/resources/cookie'});
 
 const app = document.createElement('div');
 document.body.appendChild(app);
